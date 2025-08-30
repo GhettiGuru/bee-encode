bb182427300decf5ea5a98757ec8c2af8dde5b06
Resinous <i.want.it.to.turn.black@gmail.com>
2025-08-30 00:36:08 -0500

    Update BencodingUtils.java

--- src/main/java/org/ardverk/coding/BencodingUtils.java
+++ src/main/java/org/ardverk/coding/BencodingUtils.java
@@ -1,67 +1,58 @@
 /*
  * Copyright 2009-2012 Roger Kapsi
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.ardverk.coding;
 
 /**
- * Just a few constants and utility methods
+ * Just a few constants and utility methods for Bencoding (Bee-Encoded) data.
+ *
+ * <p>Bencoding formats:</p>
+ * <ul>
+ *   <li>Dictionary: {@code d<key><value>e}</li>
+ *   <li>List: {@code l<value>e}</li>
+ *   <li>Integer: {@code i<number>e}</li>
+ *   <li>Byte strings: {@code <length>:<string>}</li>
+ * </ul>
  */
 public class BencodingUtils {
 
-  /**
-   * Constant for UTF-8 String Encoding
-   */
+  /** Constant for UTF-8 String encoding */
   public static final String UTF_8 = "UTF-8";
-  
-  /**
-   * length:bytes
-   */
+
+  /** Delimiter for byte strings of the form {@code <length>:<string>} */
   public static final int LENGTH_DELIMITER = ':';
-  
-  /**
-   * d<key><value>e
-   */
+
+  /** Dictionary start marker: {@code d<key><value>e} */
   public static final int DICTIONARY = 'd';
-  
-  /**
-   * l<value>e
-   */
+
+  /** List start marker: {@code l<value>e} */
   public static final int LIST = 'l';
-  
-  /**
-   * i<number>e
-   */
+
+  /** Number start marker: {@code i<number>e} */
   public static final int NUMBER = 'i';
-  
-  /**
-   * Marks the end of a {@link #DICTIONARY}, {@link #LIST} or {@link #NUMBER}.
-   */
+
+  /** Marks the end of a {@link #DICTIONARY}, {@link #LIST}, or {@link #NUMBER} */
   public static final int EOF = 'e';
 
-  /**
-   * Boolean values are not really supported by Bencoding but 
-   * we can write 1s for true.
-   */
+  /** Boolean true representation (Bencoding does not support booleans directly) */
   public static final Integer TRUE = Integer.valueOf(1);
 
-  /**
-   * Boolean values are not really supported by Bencoding but 
-   * we can write 0s for false.
-   */
+  /** Boolean false representation (Bencoding does not support booleans directly) */
   public static final Integer FALSE = Integer.valueOf(0);
-  
+
+  /** Private constructor to prevent instantiation */
   private BencodingUtils() {}
 }
