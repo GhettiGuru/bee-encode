3fd2a59d78c6851741814d5c4ed21d27d4a9c936
Resinous <i.want.it.to.turn.black@gmail.com>
2025-08-30 00:41:49 -0500

    Update BencodingTest.java

--- src/test/java/org/ardverk/coding/BencodingTest.java
+++ src/test/java/org/ardverk/coding/BencodingTest.java
@@ -14,58 +14,58 @@ import org.junit.Test;
 
 public class BencodingTest {
 
-	ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-	BencodingOutputStream bEncoder = new BencodingOutputStream(byteStream);
-	
-	private BencodingInputStream decoder() {
-		return decoder(false);
-	}
-	
-	private BencodingInputStream decoder(boolean decodeAsString) {
-		final byte[] bytes = byteStream.toByteArray();
-		System.out.println(new String(bytes));
-		return new BencodingInputStream(new ByteArrayInputStream(bytes), decodeAsString);
-	}
-	
-	@Before
-	public void setup() {
-		byteStream = new ByteArrayOutputStream();
-		bEncoder = new BencodingOutputStream(byteStream);
-	}
-	
-	@Test
-	public void testString() throws IOException {
-		bEncoder.writeString("test");
-		String result = decoder().readString();
-		assertEquals("test", result);
-	}
-	
-	@Test
-	public void testInt() throws IOException {
-		bEncoder.writeInt(1);
-		int result = decoder().readInt();
-		assertEquals(1, result);
-	}
-	
-	@Test
-	public void testCollectionsDecoding() throws IOException {
-		ArrayList<Object> list = new ArrayList<Object>();
-		list.add("testA");
-		list.add("testB");
-		bEncoder.writeCollection(list);
-		
-		List<Object> result = decoder(true).readList();
-		assertEquals(list, result);
-	}
-	
-	@Test
-	public void testMapDecoding() throws IOException {
-		Map<String, String> map = new TreeMap<String, String>();
-		map.put("keyA", "valueA");
-		map.put("keyB", "valueB");
-		bEncoder.writeMap(map);
-		
-		Map<String, Object> result = decoder(true).readMap();
-		assertEquals(map, result);
-	}
+    private ByteArrayOutputStream byteStream;
+    private BencodingOutputStream bEncoder;
+
+    @Before
+    public void setup() {
+        byteStream = new ByteArrayOutputStream();
+        bEncoder = new BencodingOutputStream(byteStream);
+    }
+
+    private BencodingInputStream decoder() {
+        return decoder(false);
+    }
+
+    private BencodingInputStream decoder(boolean decodeAsString) {
+        final byte[] bytes = byteStream.toByteArray();
+        System.out.println(new String(bytes));
+        return new BencodingInputStream(new ByteArrayInputStream(bytes), decodeAsString);
+    }
+
+    @Test
+    public void testString() throws IOException {
+        bEncoder.writeString("test");
+        String result = decoder().readString();
+        assertEquals("test", result);
+    }
+
+    @Test
+    public void testInt() throws IOException {
+        bEncoder.writeInt(1);
+        int result = decoder().readInt();
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testCollectionsDecoding() throws IOException {
+        List<Object> list = new ArrayList<>();
+        list.add("testA");
+        list.add("testB");
+        bEncoder.writeCollection(list);
+
+        List<Object> result = decoder(true).readList();
+        assertEquals(list, result);
+    }
+
+    @Test
+    public void testMapDecoding() throws IOException {
+        Map<String, String> map = new TreeMap<>();
+        map.put("keyA", "valueA");
+        map.put("keyB", "valueB");
+        bEncoder.writeMap(map);
+
+        Map<String, Object> result = decoder(true).readMap();
+        assertEquals(map, result);
+    }
 }
